7d39cf5d911f6ee2ff01e9a361968ff094da0c8b
Khepry Quixote <fracking.analysis@gmail.com>
2017-11-08 11:50:49 -0600

    Update utl_Create_Audit_Trigger_on_Tables.sql

--- utl_Create_Audit_Trigger_on_Tables.sql
+++ utl_Create_Audit_Trigger_on_Tables.sql
@@ -2,11 +2,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
--- Author:		  David Darling
+-- Author:	David Darling
 -- Create date: 2017-11-07
 -- Description:	Table-by-table, create an audit
---				      trigger for each user-table
+--		trigger for each user-table
 -- =============================================
 CREATE PROCEDURE [dbo].[utl_Create_Audit_Trigger_on_Tables]
 	@data_schema_name sysname = 'dbo', 
@@ -14,7 +15,7 @@ CREATE PROCEDURE [dbo].[utl_Create_Audit_Trigger_on_Tables]
 	@data_trigger_suffix sysname = 'Auditor',
 	@audit_schema_name sysname = 'auditing',
 	@audit_table_name sysname = 'Audit',
-	@primary_key_column_name sysname = 'ID',
+	@drop_existing_audit_trigger tinyint = 0,
 	@raiseMsgsAsErrors tinyint = 1
 AS
 BEGIN
@@ -24,40 +25,74 @@ BEGIN
 
 	DECLARE	@return_value int;
 	DECLARE @data_table_name sysname;
+	DECLARE @primary_key_column_name sysname;
 	DECLARE @myCursor CURSOR;
+	DECLARE @trigger_name sysname;
+	DECLARE @sql nvarchar(max);
 
 	-- TYPE can be 'U' for USER_TABLE, 'V' for VIEW, and 'IF' for INLINE-TABLE-VALUE-FUNCTION
 	-- SET @myCursor = CURSOR FOR SELECT [name] FROM sys.objects WHERE TYPE IN ('U') AND left([name],3) != 'sys';
 	SET @myCursor = CURSOR FOR
-						SELECT [TABLE_NAME]
-						FROM INFORMATION_SCHEMA.TABLES
-						WHERE TABLE_TYPE in ('BASE TABLE')
-							  and TABLE_SCHEMA in (@data_schema_name);
+						SELECT
+							t.TABLE_NAME,
+							k.COLUMN_NAME
+						FROM
+							INFORMATION_SCHEMA.TABLES t
+						INNER JOIN
+							INFORMATION_SCHEMA.KEY_COLUMN_USAGE k
+						ON
+							t.TABLE_CATALOG = k.TABLE_CATALOG
+							and
+							t.TABLE_SCHEMA = k.TABLE_SCHEMA
+							and
+							t.TABLE_NAME = k.TABLE_NAME
+						WHERE t.TABLE_SCHEMA in (@data_schema_name)
+						AND t.TABLE_TYPE in ('BASE TABLE')
+						AND LEFT(t.TABLE_NAME,3) != 'sys'
+						AND LEFT(k.CONSTRAINT_NAME,3) = 'PK_';
 							  	
 	OPEN @myCursor;
-	FETCH NEXT FROM @myCursor INTO @data_table_name;
+	FETCH NEXT FROM @myCursor INTO @data_table_name, @primary_key_column_name;
 	
 	while @@FETCH_STATUS = 0
 	BEGIN
 
-		EXEC	@return_value = [dbo].[utl_Create_Audit_Trigger_on_Table]
-				@data_schema_name = @data_schema_name,
-				@data_table_name = @data_table_name,
-				@data_trigger_prefix = @data_trigger_prefix,
-				@data_trigger_suffix = @data_trigger_suffix,
-				@audit_schema_name = @audit_schema_name,
-				@audit_table_name = @audit_table_name,
-				@primary_key_column_name = @primary_key_column_name;
+		SET @trigger_name = @data_trigger_prefix+'_'+@data_table_name+'_'+@data_trigger_suffix;
 
-		SELECT	'Data Table Name' = @data_table_name, 'Return Value' = @return_value;
+		IF (@drop_existing_audit_trigger = 1)
+		BEGIN
+			SET @sql = 'DROP TRIGGER IF EXISTS ' + @trigger_name;
+			PRINT @sql;
+			EXEC sp_executesql @sql;
+		END
 
-		-- IF (ISNULL(@raiseMsgsAsErrors,0)) = 1 RAISERROR (@sql, 0, 1) WITH NOWAIT ELSE PRINT @sql;
+		
+		IF NOT EXISTS (select name from sys.triggers where name = @trigger_name)
+		BEGIN
+			EXEC	@return_value = [dbo].[usp_Create_Audit_Trigger_on_Table]
+					@data_schema_name = @data_schema_name,
+					@data_table_name = @data_table_name,
+					@data_trigger_prefix = @data_trigger_prefix,
+					@data_trigger_suffix = @data_trigger_suffix,
+					@audit_schema_name = @audit_schema_name,
+					@audit_table_name = @audit_table_name,
+					@primary_key_column_name = @primary_key_column_name;
+			PRINT 'Trigger ' + @trigger_name + ' created successfully!';
+			PRINT '';
 
-		FETCH NEXT FROM @myCursor INTO @data_table_name;
+			SELECT	'Data Table Name' = @data_table_name, 'Return Value' = @return_value;
+
+			-- IF (ISNULL(@raiseMsgsAsErrors,0)) = 1 RAISERROR (@sql, 0, 1) WITH NOWAIT ELSE PRINT @sql;
+		END
+		ELSE
+		BEGIN
+			PRINT 'Trigger ' + @trigger_name + ' not created as it already exists!';
+		END
+
+		FETCH NEXT FROM @myCursor INTO @data_table_name, @primary_key_column_name;
 	END;
 	
 	CLOSE @myCursor;
 	DEALLOCATE @myCursor; 
 
 END
-GO
